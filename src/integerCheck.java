677f02485148bbd7b980969de21338ab7455e147
Ghazanfar_ali <salam032136muet@gmail.com>
2023-05-22 06:30:36 -0700

    add commit

--- src/integerCheck.java
+++ src/integerCheck.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 public class integerCheck {
     public static void main(String args[]) {
-//        Scanner obj1 = new Scanner(System.in);
-//        System.out.println("enter the number for checking: ");
-//        String userInput = obj1.next();
-//
-//        try {
-//            int user = Integer.parseInt(userInput);
-//            if(user % 1 ==  0) {
-//                System.out.println("number is integer");
-//            }
-//        }catch(Exception e){
-//            System.out.println("number is not integer");
-//        //    System.out.println(e.getLocalizedMessage());
-//            }
+        Scanner obj1 = new Scanner(System.in);
+        System.out.println("enter the number for checking: ");
+        String userInput = obj1.next();
+
+        try {
+            int user = Integer.parseInt(userInput);
+            if(user % 1 ==  0) {
+                System.out.println("number is integer");
+            }
+        }catch(Exception e){
+            System.out.println("number is not integer");
+        //    System.out.println(e.getLocalizedMessage());
+            }
 
 
 
